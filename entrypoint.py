b53617397e4ad186892ff3af44ef67e8739ec9b8
Jeremy Underwood <underwood.j248@gmail.com>
2024-06-04 13:26:29 -0400

    From the /collect endpoint return a dictionary containing the most recent and average engine temperature

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,7 @@
 import json
 import redis as redis
 # import the flask web framework
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from loguru import logger
 
 # constants
@@ -10,6 +10,7 @@ DATA_KEY = "engine_temperature"
 
 # create a Flask server, and allow us to interact with it using the app
 app = Flask(__name__)
+
 
 # define an endpoint which accepts POST requests, and is reachable from
 # the /record endpoint
@@ -22,7 +23,7 @@ def record_engine_temperature():
     engine_temperature = payload.get("engine_temperature")
     logger.info(f"engine temperature to record is: {engine_temperature}")
     # open a connection to the redis database 
-    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+    database = redis.Redis(host='redis', port=6379, decode_responses=True)
     # add key and engine temperature to head of list
     database.lpush(DATA_KEY, engine_temperature)
     logger.info(f"stashed engine temperature in redis: {engine_temperature}")
@@ -44,4 +45,19 @@ def record_engine_temperature():
 # from the /collect endpoint
 @app.route('/collect', methods=['POST'])
 def collect_engine_temperature():
-    return{"success": True}, 200
+    # open a connection to the redis database 
+    database = redis.Redis(host='redis', port=6379, decode_responses=True)
+    # assign the elements of the list stored at key to variable and start at the first element and end with the last element
+    engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
+    # assign the most recent engine temperature reading in the database to a variable
+    current_engine_temperature = engine_temperature_values[0]
+    logger.info(f"current engine temperature: {current_engine_temperature}")
+    # compute and assign the mean engine temperature drived from
+    # the database
+    average_engine_temperature = sum(engine_temperature_values) / len(engine_temperature_values)
+    logger.info(f"average engine temperature: {average_engine_temperature}")
+    # return a dictionary containing the engine current and average temperatures
+    return{
+        "current_engine_temperature": current_engine_temperature,
+        "average_engine_temperature": average_engine_temperature
+        }
